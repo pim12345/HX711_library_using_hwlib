d83f32d374c239856b9c97fcd4d20d56f021982f
pim12345 <pim.goes@student.hu.nl>
2020-06-23 11:29:02 +0200

    update tried communicating with hx711

--- weegschaal_code/main.cpp
+++ weegschaal_code/main.cpp
@@ -1,15 +1,41 @@
-#include "hwlib.hpp"
+//#include "hwlib.hpp"
 #include "../library_code/hx711.hpp"
 #include <vector>
 #include <array>
-#include <iostream>
-#include <ostream>
+//#include <iostream>
+//#include <ostream>
 
 
 int main( void ) {
-    auto SCK = hwlib::target::pin_out( hwlib::target::pins::d9 );
-    auto DT = hwlib::target::pin_in( hwlib::target::pins::d9 );
+    hwlib::cout << "begin" << '\n';
+    auto SCK = hwlib::target::pin_out( hwlib::target::pins::d10 );
+    auto DT = hwlib::target::pin_in( hwlib::target::pins::d6 );
 
     auto weegschaal = hx711(SCK, DT);
-
-}
+    for (;;){
+    SCK.write(0);
+    SCK.flush();
+    hwlib::wait_ms(1);
+    DT.refresh();
+    hwlib::cout << DT.read() << '\n';
+    while(DT.read() == 1){
+      DT.refresh();
+      hwlib::cout << DT.read() << '\n';
+    }
+      for (int i=0; i<25; i++){
+        SCK.write(1);
+        SCK.flush();
+        SCK.write(0);
+        SCK.flush();
+        DT.refresh();
+        hwlib::cout << DT.read() << '\n';
+        //hwlib::wait_us();
+        hwlib::wait_us(1);
+      }
+      SCK.write(1);
+      SCK.flush();
+      SCK.write(0);
+      SCK.flush();
+      //hwlib::
+    }
+    }
